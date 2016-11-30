910b4cf4f3840620e10e566278a18f6825a80db9
tommy932 <tommy.931@live.it>
2016-11-30 11:26:50 +0100

    Test coverage 74%

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -16,8 +16,8 @@ public class GildedRose {
         System.out.println("OMGHAI!");
 		
         items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("+5 Dexterity Vest", 0, 20));
+        items.add(new Item("Aged Brie", 2, -1));
         items.add(new Item("Elixir of the Mongoose", 5, 7));
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
@@ -102,6 +102,10 @@ public class GildedRose {
                 }
             }
         }
+    }
+    
+    public static List<Item> getItems(){
+    	return items;
     }
 
 }
